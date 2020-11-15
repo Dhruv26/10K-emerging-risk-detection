a6e9f5620145d733e1efd6eb1263445abd83964f
Dhruv Segat <dhruv.segat@matillion.com>
2020-11-15 22:57:54 +0000

    Fix logging and import issues

--- preprocessing/__init__.py
+++ preprocessing/__init__.py
@@ -1,4 +1,9 @@
 import logging
 
 
-logging.basicConfig(filename='risk_extractor.log')
+logging_format = '%(asctime)s — %(name)s — %(levelname)s — %(funcName)s:%(' \
+                 'lineno)d — %(message)s'
+logging.basicConfig(
+    filename='risk_extractor.log',
+    format=logging_format
+)
--- preprocessing/zip_processor.py
+++ preprocessing/zip_processor.py
@@ -4,7 +4,7 @@ from zipfile import ZipFile
 import time
 import logging
 
-from .report_parser import extract_risk_section_from_report
+from report_parser import extract_risk_section_from_report
 
 LOGGER = logging.getLogger(__name__)
 
@@ -24,13 +24,23 @@ def write_risk_section_to_file(zipfile_path, output_dir):
                 error_msg = f'Error occurred while trying to extract risk ' \
                             f'section for {report_file.filename} '
                 LOGGER.error(error_msg, exc_info=True)
+                continue
 
             output_file = os.path.join(output_dir, report_file.filename)
             with open(output_file, 'w+') as risk_section_file:
                 risk_section_file.write(risk_section)
+            LOGGER.info(f'Extracted risk section for {report_file.filename}')
 
 
 if __name__ == '__main__':
+    # TODO: Configure logging at project level
+    logging_format = '%(asctime)s — %(name)s — %(levelname)s — %(funcName)s' \
+                     ':%(lineno)d — %(message)s'
+    logging.basicConfig(
+        filename='risk_extractor.log',
+        format=logging_format
+    )
+
     input_zipfile = 'project_dataset_full.zip'
     output_dir = 'risk_section'
     LOGGER.info(f'Processing {input_zipfile}')
@@ -40,4 +50,5 @@ if __name__ == '__main__':
     time_taken = time.time() - start
 
     LOGGER.info(
-        f'Written result to {output_dir}. Took {time_taken:.2f} seconds.')
+        f'Written result to {output_dir}. Took {time_taken:.2f} seconds.'
+    )
