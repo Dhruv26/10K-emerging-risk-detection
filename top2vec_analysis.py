0286576ee1965015ec76457eae4c0f6c923f50cb
Dhruv Segat <dhruvsegat18@gmail.com>
2020-12-08 21:47:00 +0530

    Fix issues in top2vec
    Move requirements to setup.py from requirements.txt.

--- top2vec_analysis.py
+++ top2vec_analysis.py
@@ -1,7 +1,6 @@
 import glob
 import os
 
-# TODO: Update requirements
 from nltk.tokenize import word_tokenize
 from top2vec import Top2Vec
 from tqdm import tqdm
@@ -9,7 +8,7 @@ from tqdm import tqdm
 from config import Config
 
 if __name__ == '__main__':
-    risk_dir = os.path.join(Config.risk_dir(), '10k20f_5')
+    risk_dir = os.path.join(Config.risk_dir(), '10k20f_5', '*.txt')
     print(f'Reading files from {risk_dir}')
     risk_files = glob.glob(risk_dir)
 
@@ -22,6 +21,6 @@ if __name__ == '__main__':
     print(f'Read {len(corpus)} files.')
 
     model = Top2Vec(corpus, speed='deep-learn', workers=16)
-    model_path = os.path.join(Config.models_dir(), 'top2vec_model')
+    model_path = os.path.join(Config.models_dir(), 'top2vec_model_deep')
     model.save(model_path)
     print(f'Saved model to {model_path}')
